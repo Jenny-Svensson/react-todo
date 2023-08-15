7a97737d2426641b67b7af014777c6a7c016f51f
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-08-15 12:18:34 +0200

    display the coded todos using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import TodoList from './components/TodoList';
+import { Todo } from './models/Todo';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { // todos = all of our todos, setTodos = function to update our todos
+  const [todos, setTodos] = useState<Todo[]>([
+    
+    new Todo("Test 1", false, 1),
+    new Todo("Test 2", false, 2),
+    new Todo("Test 3", true, 3),
+  ])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <> 
+      <TodoList todos={todos} />
+      <input type="text" /> 
+      <button>Add</button>
     </>
-  )
-}
+    )
+  }
+  
 
-export default App
+export default App;
