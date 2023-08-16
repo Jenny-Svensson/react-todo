d71fcc37720bcdaec1bd1f5962c3a59d4b011cb9
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-08-16 09:57:01 +0200

    toggle a todo to done or not done (false or true)

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,14 +2,15 @@ import { Todo } from "../models/Todo"
 
 interface ItodoListProps {
   todos: Todo[];
+  onToggle: (id: string) => void;
 }
 
-export const TodoList = ({ todos }: ItodoListProps) => {
+export const TodoList = ({ todos, onToggle }: ItodoListProps) => {
   return (
     <ul>
       {todos.map((todo) => (
         <li key={todo.id}>
-          <input type="checkbox" checked={todo.done} readOnly />
+          <input type="checkbox" checked={todo.done} onChange={() => onToggle(todo.id)} />
           {todo.text}
         </li>
       ))}
