965d42ded9beda6f5783fa301a4dcb894596bdf4
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-08-25 09:47:22 +0200

    a little clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,8 @@ import { Todo } from './models/Todo';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([
-    //todos = all of our todos, 
-    //setTodos = function to update our todos
-    new Todo("Test 1", false, uuidv4()),
-    new Todo("Test 2", false, uuidv4()),
-    new Todo("Test 3", true, uuidv4()),
-  ])
-
-  //add new todos to the todoList with setTodos
-  const [newTodo, setNewTodo] = useState<string>(""); // create a useState to save the new todos
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [newTodo, setNewTodo] = useState<string>("");
 
   const addTodo = () => {
     if (newTodo.trim() !== "") { // if newTodo is not empty string, continue
@@ -33,7 +25,6 @@ function App() {
   };
 
   const handleDelete = (id: string) => {
-    // Filter out the todo item with the given id
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
   };
